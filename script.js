b3100242c5dc8a428cc1c46bf25adf8be41beba5
MSebb23 <matteosabbatini23@gmail.com>
2021-04-20 01:28:08 +0200

    Scaffolding Project : HEALTH

---
+++ script.js
@@ -0,0 +1,21 @@
+var vue_box = new Vue(
+    {
+
+    el : "#vue_box",
+    data :{
+        nav_links : [
+            
+        ],
+        index : 0,
+    },
+    methods:{
+
+    },
+    mounted () {
+        console.log("23");
+    },
+    }
+
+
+)
+// alert("hello");
